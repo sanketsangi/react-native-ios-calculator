778f0268e7a664155b0cafdae4b9f470a3645d3a
signuldotdev <sanket.prog@gmail.com>
2022-10-12 20:21:51 +0530

    Row Component Created

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const Row = () => {
+const Row = ({ children }) => {
   return (
-    <View>
-      <Text>Row</Text>
+    <View
+      style={{
+        flexDirection: "row",
+      }}
+    >
+      {children}
     </View>
   );
 };
