0ff2cac754d66e451c009ced6cddf40e0c32e9c4
signuldotdev <sanket.prog@gmail.com>
2022-10-12 20:30:54 +0530

    Button Create

--- App.js
+++ App.js
@@ -11,10 +11,75 @@ export default function App() {
         Value
       </Text>
       <Row>
-        <Button text="C" onPress={() => this.handleTap("clear")} />
-        <Button text="+/-" onPress={() => this.handleTap("clear")} />
-        <Button text="%" onPress={() => this.handleTap("clear")} />
-        <Button text="/" onPress={() => this.handleTap("clear")} />
+        <Button
+          text="C"
+          theme="secondary"
+          onPress={() => this.handleTap("clear")}
+        />
+        <Button
+          text="+/-"
+          theme="secondary"
+          onPress={() => this.handleTap("clear")}
+        />
+        <Button
+          text="%"
+          theme="secondary"
+          onPress={() => this.handleTap("clear")}
+        />
+        <Button
+          text="/"
+          theme="accent"
+          onPress={() => this.handleTap("clear")}
+        />
+      </Row>
+
+      <Row>
+        <Button text="7" onPress={() => this.handleTap("number", 7)} />
+        <Button text="8" onPress={() => this.handleTap("number", 8)} />
+        <Button text="9" onPress={() => this.handleTap("number", 9)} />
+        <Button
+          text="x"
+          theme="accent"
+          onPress={() => this.handleTap("operator", "*")}
+        />
+      </Row>
+      <Row>
+        <Button text="4" onPress={() => this.handleTap("number", 4)} />
+        <Button text="5" onPress={() => this.handleTap("number", 5)} />
+        <Button text="6" onPress={() => this.handleTap("number", 6)} />
+        <Button
+          text="-"
+          theme="accent"
+          onPress={() => this.handleTap("operator", "-")}
+        />
+      </Row>
+      <Row>
+        <Button text="7" onPress={() => this.handleTap("number", 7)} />
+        <Button text="8" onPress={() => this.handleTap("number", 8)} />
+        <Button text="9" onPress={() => this.handleTap("number", 9)} />
+        <Button
+          text="+"
+          theme="accent"
+          onPress={() => this.handleTap("operator", "+")}
+        />
+      </Row>
+
+      <Row>
+        <Button
+          text="0"
+          size="double"
+          onPress={() => this.handleTap("number", 0)}
+        />
+        <Button
+          text="."
+          size="double"
+          onPress={() => this.handleTap("operator", ".")}
+        />
+        <Button
+          text="="
+          theme="accent"
+          onPress={() => this.handleTap("equal")}
+        />
       </Row>
       <StatusBar style="auto" />
     </SafeAreaView>
